43b547fde8eb4802faf6096f04be5bbeae892e86
Dusan Kmet <morlokdk@gmail.com>
2017-07-13 10:53:46 +0200

    git ignore .idea

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,13 @@
 var gulp = require('gulp');
 var minify = require('gulp-minify');
 
-
 gulp.task('default', function () {
-	gulp.src('./js/*.js')
-	    .pipe(minify({
-	        ext:{
-	            min:'-min.js'
-	        },
-	        exclude: ['tasks'],
-	        noSource: true,
-	        mangle: true
-	    }))
-    	.pipe(gulp.dest('./src/assets/dist/js'));
+	return gulp.src('./js/*.js')
+		.pipe(minify({
+			ext: {min: '-min.js'},
+			exclude: ['tasks'],
+			noSource: true,
+			mangle: true
+		}))
+		.pipe(gulp.dest('./src/assets/dist/js'));
 });
